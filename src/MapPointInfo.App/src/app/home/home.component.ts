72774fd44f2a6bce81a1803340d08390310ffb67
Webster <atkseegow@gmail.com>
2024-03-19 02:04:28 +0800

    _

--- src/MapPointInfo.App/src/app/home/home.component.ts
+++ src/MapPointInfo.App/src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { GoogleMap, GoogleMapsModule, MapMarker } from '@angular/google-maps'
+import { GoogleMap, GoogleMapsModule, MapAdvancedMarker } from '@angular/google-maps'
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [GoogleMapsModule, HttpClientModule, CommonModule],
+  imports: [GoogleMapsModule, HttpClientModule, CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -14,12 +16,22 @@ export class HomeComponent implements AfterViewInit {
   @ViewChild(GoogleMap, { static: false }) map: GoogleMap | undefined
 
   markers: any[] = [];
+  markerInfos: any[] = [];
+  
+  selectedMarkerInfos: any[] = [];
+  selectedMarkerInfo: any = { "youTubeLink": "https://www.youtube.com/embed/mmujjwPHa10?si=QsNpnU8vost4tP6I"};
+  selectedMarkerInfoDateTimes: string[] = [];
 
-  constructor(private http: HttpClient) {
-   
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {
     this.http.get<any>('assets/data/markers.json').subscribe(data => {
       data.forEach((item: any) => {
         this.markers.push(item)
+      });
+    });
+
+    this.http.get<any>('assets/data/markerInfos.json').subscribe(data => {
+      data.forEach((item: any) => {
+        this.markerInfos.push(item)
       });
     });
   }
@@ -33,7 +45,22 @@ export class HomeComponent implements AfterViewInit {
   };
   zoom = 8;
 
-  aa(marker: any){
-    alert(marker.title)
+  getSelectedMarkerInfos(marker: any){
+    this.selectedMarkerInfos = this.markerInfos.filter(item => marker.title == item.title);
+    this.selectedMarkerInfoDateTimes = []
+    this.selectedMarkerInfos.forEach((item: any) => {
+      this.selectedMarkerInfoDateTimes.push(item.dateTime)
+    });
+    this.getSelectedMarkerInfo({ "target": { "value": this.selectedMarkerInfoDateTimes[0] }});
+  }
+
+  getSelectedMarkerInfo(event: any)
+  {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    this.selectedMarkerInfo = this.selectedMarkerInfos.filter(item => item.dateTime == selectedValue)[0];
+  }
+
+  bypassSecurityTrustResourceUrl(url: string){
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
